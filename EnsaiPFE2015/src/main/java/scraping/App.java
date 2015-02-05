618d4817580b3a2e830531650f25f19a89062779
cBremardAdvalo <cbremard@advalo.com>
2015-02-05 11:20:14 +0100

    first exemple
    Get meta data from product page

--- EnsaiPFE2015/src/main/java/scraping/App.java
+++ EnsaiPFE2015/src/main/java/scraping/App.java
@@ -1,13 +1,36 @@
 package scraping;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	LOGGER.debug("START");
+        try {
+			Document doc = Jsoup.connect("http://www.aubert.com/Lits-60-x-120-cm-transformables-Lit-Sleepi-120-cm-Noyer-Stokke.html").get();
+			String data = "For product page \"/www.aubert.com/Lits-60-x-120-cm-transformables-Lit-Sleepi-120-cm-Noyer-Stokke.html\", we have meta data :";
+			for(Element meta : doc.select("meta")) {
+				if(meta.hasAttr("itemprop")){
+					data += "\r\n\t\""+meta.attr("itemprop")+"\" = "+meta.attr("content");
+				}
+			}
+			LOGGER.info(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	LOGGER.debug("END");
     }
 }
